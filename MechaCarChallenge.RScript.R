868b7b1224ede8d78a17b9659bb19f26fde0909e
suyinwb <wong.suyin@gmail.com>
2022-02-18 17:07:53 -0500

    D2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -13,3 +13,25 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD,data=MechaCar_mpg))
+
+###################################################################################
+
+################################# DELIVERABLES 2 #################################
+
+Suspension_Coil <- read.csv('resources/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+#create summary table 
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), 
+                                               Median=median(PSI), 
+                                               Variance=var(PSI), 
+                                               SD=sd(PSI), 
+                                               .groups = 'keep')
+
+#create summary table with multiple columns
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(
+  Mean=mean(PSI), 
+  Median=median(PSI), 
+  Variance=var(PSI), 
+  SD=sd(PSI), 
+  .groups = 'keep')
+
